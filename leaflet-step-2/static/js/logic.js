745819d95f061d57337d8a892a5ba02e9efde36b
unknown <jgabunil@ucla.edu>
2020-09-27 00:34:39 -0700

    Fixed legend

--- leaflet-step-2/static/js/logic.js
+++ leaflet-step-2/static/js/logic.js
@@ -97,12 +97,22 @@ var createMap = function(earthquakeMarkers, plateMarkers) {
                 id: "mapbox/dark-v10",
                 accessToken: API_KEY
         });
+
+        var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+                tileSize: 512,
+                maxZoom: 18,
+                zoomOffset: -1,
+                id: "mapbox/satellite-v9",
+                accessToken: API_KEY
+        });
         
 
         // Add the base layers
         var baseMaps = {
                 "Light Map": lightmap,
-                "Dark Map": darkmap
+                "Dark Map": darkmap,
+                "Geographical Map": satellitemap
         };
 
         // Add the overlay layers
@@ -125,7 +135,7 @@ var createMap = function(earthquakeMarkers, plateMarkers) {
                 position: "bottomleft"
         });
         info.onAdd = function() {
-                var div = L.DomUtil.create("div", "legend");
+                var div = L.DomUtil.create("div", "info legend");
                 var limits = [
                         '<0',
                         '0 - 39',
